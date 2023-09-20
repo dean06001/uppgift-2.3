5c5b5c61686652014ce55b1845d925bd7ace7927
dean06001 <dean06001@E-PW0234KC>
2023-09-20 10:37:36 +0200

    Add project files.

---
+++ uppgift 2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift_2_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge ska du hyra bilen?");
+            double Hyra = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur¨många kilometer ska du köra?");
+            double kilometer = double.Parse(Console.ReadLine());
+
+            double hyraSumma = 300 + (Hyra - 1) * 500;
+            double kmSumma = kilometer * 1;
+
+            double totalaSumma = hyraSumma + kmSumma;
+            Console.WriteLine("Det kommer kosta dig " + totalaSumma + "kr.");
+            Console.ReadKey();
+
+        }
+    }
+}
